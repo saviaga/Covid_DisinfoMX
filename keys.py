d10e97ed00ccf7a150b0dd257919ed324893bdb7
saviaga <saviaga@hotmail.com>
2020-06-21 16:19:05 -0400

    keys

---
+++ keys.py
@@ -0,0 +1,7 @@
+rapidapi_key = "xxxx" # now it's called rapidapi key
+twitter_app_auth = {
+    'consumer_key': 'xxxxx',
+    'consumer_secret': 'xxxx',
+    'access_token': 'xxx',
+    'access_token_secret': 'xxx',
+  }
